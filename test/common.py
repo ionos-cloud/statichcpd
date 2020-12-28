f99047cfe1493a9dda3e6ccb45fb575503d26ba7
Reshma Sreekumar <reshma.sreekumar@cloud.ionos.com>
2020-12-28 15:57:36 +0100

    Added DHCPv6 to basic testing

--- test/common.py
+++ test/common.py
@@ -19,30 +19,11 @@ conn = None
 class server_process():
     def __init__(self, cfg=None):
         self.process = None
-        self.conf_file = cfg.conf_file if cfg else '/tmp/statichcpd.conf'
+        self.conf_file = cfg.conf_file
         self.nsname = cfg.server_nsname if cfg else 'dummy_dhcp_ns'
         
-        
-        # Setup a new conf file in /tmp location
-        if os.path.exists(self.conf_file):
-            os.remove(self.conf_file)
-        with open('/etc/statichcpd/statichcpd.conf') as f:
-            with open(self.conf_file, "a") as f1:
-                for line in f:
-                    f1.write(line)
-
-        # Add non-default dhcp database file path and regex
-        with open(self.conf_file, "a") as f1:
-            f1.write('dhcp_db_filename = /tmp/Static_DHCP_DB.db\n')
-            if all(re.match('^vethserver.*', intf.name) for intf in cfg.servers): 
-                f1.write('served_interface_regex = ^vethserver.*\n')
-            else:
-                # If the interface names are different from default
-                # user needs to mandatorily supply the regex
-                f1.write('served_interface_regex = ' + cfg.get('regex') + "\n")
-
     def __enter__(self):
-        bashcommand = "python3 -m statichcpd -c /tmp/statichcpd.conf"
+        bashcommand = "python3 -m statichcpd -c " + self.conf_file
         self.process = NSPopen(self.nsname, bashcommand.split(), stdout=subprocess.PIPE)
         return self
 
@@ -56,7 +37,7 @@ class server_process():
         self.process.wait()
         self.process.stdout.close()
 
-class packet_sender():
+class dhcp_sender():
     def __init__(self, args):
         self.process = None
         self.argstr = args
@@ -73,14 +54,16 @@ class packet_sender():
         self.process.stdout.close()
 
 class Interface():
-    def __init__(self, name=None, addr=None):
+    def __init__(self, name=None, addr4=None, addr6=None):
         self.name = name
-        self.addr = addr
+        self.addr4 = addr4
+        self.addr6 = addr6
 
 class Config():
     def __init__(self, arg_dict={}):
         self.server_nsname = arg_dict.get('server_nsname', 'dummy_dhcp_ns')
         self.conf_file = arg_dict.get('conf_file', '/tmp/statichcpd.conf')
+        self.dbname = '/tmp/Static_DHCP_DB.db'
         self.server_ns = NetNS(self.server_nsname)
         self.ip = None
         self.count = arg_dict.get('count', 1)
@@ -88,11 +71,30 @@ class Config():
         self.servers = [Interface(name=name) for name in ['vethserver' + str(idx) for idx in range(self.count)]]
         if 'server_ips' in arg_dict and len(arg_dict['server_ips']) >= self.count:
             for idx in range(self.count):
-                self.servers[idx].addr = arg_dict['server_ips'][idx]
+                self.servers[idx].addr4 = arg_dict['server_ips'][idx]
         else:
             for idx in range(self.count):
-                self.servers[idx].addr = str((idx + 2) * 10) + '.0.0.1'
-        self.client_conf = arg_dict.get('client_conf', None)
+                self.servers[idx].addr4 = str((idx + 2) * 10) + '.0.0.1'
+        self.client_v4conf = arg_dict.get('client_v4conf', None)
+        self.client_v6conf = arg_dict.get('client_v6conf', None)
+
+        # Setup a new conf file in /tmp location
+        if os.path.exists(self.conf_file):
+            os.remove(self.conf_file)
+        with open('/etc/statichcpd/statichcpd.conf') as f:
+            with open(self.conf_file, "a") as f1:
+                for line in f:
+                    f1.write(line)
+
+        # Add non-default dhcp database file path and regex
+        with open(self.conf_file, "a") as f1:
+            f1.write('dhcp_db_filename = ' + self.dbname + '\n')
+            if all(re.match('^vethserver.*', intf.name) for intf in self.servers): 
+                f1.write('served_interface_regex = ^vethserver.*\n')
+            else:
+                # If the interface names are different from default
+                # user needs to mandatorily supply the regex
+                f1.write('served_interface_regex = ' + arg_dict.get('regex') + "\n")
 
     def __enter__(self):
         self.ip = IPRoute()
@@ -100,7 +102,6 @@ class Config():
             for i in range(self.count):
                 # Create client-server veth pairs
                 self.ip.link('add', ifname=self.clients[i].name, kind='veth', peer=self.servers[i].name)
-
                 # Set the client interface to UP
                 idx = self.ip.link_lookup(ifname=self.clients[i].name)[0]
                 self.ip.link('set', index=idx, state='up')
@@ -112,7 +113,7 @@ class Config():
                 # Add IP address to the server interface
 
                 idx = self.ip.link_lookup(ifname=self.servers[i].name)[0]
-                self.ip.addr('add', idx, address=self.servers[i].addr, mask=24)
+                self.ip.addr('add', idx, address=self.servers[i].addr4, mask=24)
               
                 # Move the server interface to server namespace
                 self.ip.link('set', index=idx, net_ns_fd=self.server_nsname)
@@ -120,15 +121,18 @@ class Config():
                 # Set the interface to UP and add address again since
                 # all configs are lost on vrf move
                 self.server_ns.link('set', index=idx, state='up')
-                self.server_ns.addr('add', idx, address=self.servers[i].addr, mask=24)
+                self.server_ns.addr('add', idx, address=self.servers[i].addr4, mask=24)
 
              
             loidx = self.server_ns.link_lookup(ifname='lo')[0]
             self.server_ns.link('set', index=loidx, state='up')
-            if self.client_conf is None:
-                self.client_conf = build_default_conf(self.clients)
+            if self.client_v4conf is None:
+                self.client_v4conf = build_default_v4conf(self.clients)
+            if self.client_v6conf is None:
+                self.client_v6conf = build_default_v6conf(self.clients)
             return self
-        except NetlinkError:
+        except NetlinkError as err:
+            print("Configuration failed with error: ", err)
             if self.ip is not None:
                 self.ip.close()
                 self.ip = None
@@ -143,7 +147,14 @@ class Config():
         if self.server_ns is not None:
             self.server_ns.close()
             netns.remove(self.server_nsname)
-        del self.client_conf
+        del self.client_v4conf
+        del self.client_v6conf
+
+    def __del__(self):
+        if os.path.exists(self.conf_file):
+            os.remove(self.conf_file)
+        if os.path.exists(self.dbname):
+            os.remove(self.dbname)
 
 def getHwAddr(ifname: str) -> str:
     sock =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -188,7 +199,7 @@ def init_v6_tables(mac_list: List[str], ifname_list: List[str],
     for i in range(len(mac_list)):
         mac = mac_list[i]
         ifname = ifname_list[i]
-        attr_list = v6_attr_lists[i]
+        attr_list = v6_attr_lists[clients[i].name]
         for attr in attr_list:
             cursor.execute(""" insert into client_v6configuration
                                        (ifname, duid, attr_code, attr_val) values
@@ -225,15 +236,25 @@ def create_dhcp_database(mac: List[str], ifname: List[str],
     init_host_conf_table(mac, ifname, clients, attr_lists)
     init_v6_tables(mac, ifname, clients, v6_attr_lists)
 
-def build_default_conf(clients: List[Interface]):
-    default_conf = {}
+def build_default_v6conf(clients: List[Interface]):
+    default_v6conf = {}
+    for idx in range(len(clients)):
+        client_ip6 = str((idx + 2) * 10) + '::2'
+        client_v6prefix = str((idx + 2) * 10) + '::/64'
+        default_v6conf[clients[idx].name] = [(3, client_ip6),
+                                             (4, client_ip6),
+                                             (25, client_v6prefix)]
+    return default_v6conf
+
+def build_default_v4conf(clients: List[Interface]):
+    default_v4conf = {}
     for idx in range(len(clients)):
         client_ip = str((idx + 2) * 10) + '.0.0.2'
         subnet_mask = '255.255.255.0'
         time_offset = 0xFFFFD5D0
         hostname = 'host' + str(idx)
 
-        default_conf[clients[idx].name] = [(256, client_ip),
+        default_v4conf[clients[idx].name] = [(256, client_ip),
                                      (dhcp.DHCP_OPT_NETMASK, subnet_mask),
                                      (dhcp.DHCP_OPT_TIMEOFFSET, time_offset),
                                      (dhcp.DHCP_OPT_HOSTNAME, hostname)]
@@ -241,26 +262,20 @@ def build_default_conf(clients: List[Interface]):
 
         # Add multiple routers
         for i in range(10, 13):
-            default_conf[clients[idx].name].extend([(dhcp.DHCP_OPT_ROUTER, str((idx + 1) * 10) + '.0.0.' + str(i))])
+            default_v4conf[clients[idx].name].extend([(dhcp.DHCP_OPT_ROUTER, str((idx + 1) * 10) + '.0.0.' + str(i))])
 
         # Add classless routes
         classless_rt = [str((idx + 2)* 20) + '.1.0.0/16,' + str((idx + 2)* 20) + '.1.0.1', str((idx + 2)* 30) + '.10.10.0/24,' + str((idx + 2)* 30) + '.10.10.1']
         for rt in classless_rt:
-            default_conf[clients[idx].name].extend([(121, rt)])
-    
-    return default_conf
+            default_v4conf[clients[idx].name].extend([(121, rt)])
+
+   
+    return default_v4conf
         
 def fill_db(cfg: Config):
     server_if_list  = cfg.servers
     client_list = cfg.clients
     mac_list = [getHwAddr(client.name) for client in cfg.clients]
-    v6_attr_lists = [[(3, '20::1'),
-                  (3, '20::2'),
-                  (4, '40::1'),
-                  (25, '20::/64')],
-                 [(3, '30::1'),
-                  (3, '30::2'),
-                  (4, '50::1'),
-                  (25, '30::/64')]]
-    create_dhcp_database(mac_list, [server.name for server in cfg.servers], cfg.clients, cfg.client_conf, v6_attr_lists)
+    create_dhcp_database(mac_list, [server.name for server in cfg.servers], 
+                         cfg.clients, cfg.client_v4conf, cfg.client_v6conf)
     conn.close()
--- test/test_config.py
+++ test/test_config.py
@@ -1,17 +1,18 @@
 import unittest
 from time import sleep
 import tracemalloc
-from ipaddress import IPv4Address, AddressValueError
+from ipaddress import IPv4Address, IPv6Address, AddressValueError
 from unittest import skipUnless
 from os import geteuid
+from socket import AF_INET6
 
-from test.common import fill_db, Config, server_process, packet_sender
+from test.common import fill_db, Config, server_process, dhcp_sender
 
 tracemalloc.start()
 
 class TestStatichcpdMethods(unittest.TestCase):
     @skipUnless(geteuid() == 0, "Unable to run test without root access")
-    def test_dhcp_conf(self):
+    def test_dhcp(self):
         with Config() as cfg:
             fill_db(cfg)
             print("\nPopulated DHCP database with client configs...")
@@ -19,8 +20,10 @@ class TestStatichcpdMethods(unittest.TestCase):
                 sleep(1) # Don't rush! Let the server get ready!
 
                 for (client, server) in zip(cfg.clients, cfg.servers):
-                    with packet_sender('-v ' + client.name) as sender:
-                        sleep(1) # Give some time for the address to get configured
+
+                    # DHCPv4 Selecting-Init state
+                    with dhcp_sender('-v ' + client.name) as sender:
+                        sleep(2) # Give some time for the address to get configured
                         client_idx = cfg.ip.link_lookup(ifname=client.name)[0]
                         client_ip = cfg.ip.get_addr(index=client_idx)[0].get_attr('IFA_ADDRESS')
                         try:
@@ -29,7 +32,8 @@ class TestStatichcpdMethods(unittest.TestCase):
                         except AddressValueError:
                             self.fail("Client " + client.name + " failed to get succesful IP binding")
 
-                    with packet_sender('-v ' + client.name + ' -s ' + server.addr) as sender:
+                    # DHCPv4 Renew state
+                    with dhcp_sender('-v ' + client.name + ' -s ' + server.addr4) as sender:
                         sleep(1)
                         client_ip = cfg.ip.get_addr(index=client_idx)[0].get_attr('IFA_ADDRESS')
                         try:
@@ -37,6 +41,36 @@ class TestStatichcpdMethods(unittest.TestCase):
                             print("Client ", client.name, " succesfully renewed IP address ", client_ip)
                         except AddressValueError:
                             self.fail("Client " + client.name + " failed to renew IP address " + client_ip)
+
+                    # DHCPv6 IPv6 address request
+                    with dhcp_sender('-v -6 ' + client.name) as sender:
+                        sleep(2) # The solicit-advertisement-request-reply will take some time
+                        client_ip = None
+                        for addr in cfg.ip.get_addr(index=client_idx, family=AF_INET6):
+                            if addr['scope'] == 0:
+                                client_ip = cfg.ip.get_addr(index=client_idx, family=AF_INET6)[0].get_attr('IFA_ADDRESS')
+                                break
+                        try:
+                            client_ip = IPv6Address(client_ip)
+                            print("Client ", client.name, " succesfully configured with IPv6 address ", client_ip)
+                        except AddressValueError:
+                            self.fail("Client " + client.name + " failed to fetch IPv6 address ")
+
+                    # DHCPv6 IPv6 Prefix request
+                    with dhcp_sender('-v -6 -P ' + client.name) as sender:
+                        sleep(2) # The solicit-advertisement-request-reply will take some time
+                        client_ip = None
+                        for addr in cfg.ip.get_addr(index=client_idx, family=AF_INET6):
+                            if addr['scope'] == 0:
+                                client_ip = cfg.ip.get_addr(index=client_idx, family=AF_INET6)[0].get_attr('IFA_ADDRESS')
+                                break
+                        try:
+                            client_ip = IPv6Address(client_ip)
+                            print("Client ", client.name, " succesfully configured with IPv6 address ", client_ip)
+                        except AddressValueError:
+                            self.fail("Client " + client.name + " failed to fetch IPv6 address ")
+
+
 
 if __name__ == '__main__':
         unittest.main()
